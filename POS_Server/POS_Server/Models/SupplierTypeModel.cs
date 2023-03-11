ab2655b82f4be87010a424b7bbc7e2bbc1cea109
git-eng-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-03-11 13:12:42 +0200

    supplierTypeModel

---
+++ POS_Server/POS_Server/Models/SupplierTypeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace POS_Server.Models
+{
+    public class SupplierTypeModel
+    {
+        public int SupplierTypeId { get; set; }
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public bool IsBlocked { get; set; }
+        public bool IsActive { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+        public Nullable<long> CreateUserId { get; set; }
+        public Nullable<long> UpdateUserId { get; set; }
+    }
+}
