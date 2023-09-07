994ff0f61125686eaef0c5fe62f084980ac7d0dd
git-eng-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-09-07 16:09:13 +0300

    .

---
+++ POS_Server/POS_Server/Controllers/PermissionsController.cs
@@ -0,0 +1,39 @@
+﻿using POS_Server.Models.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Http;
+
+namespace POS_Server.Controllers
+{
+    [RoutePrefix("api/Permissions")]
+    public class PermissionsController : ApiController
+    {
+        CountriesController cc = new CountriesController();
+        [HttpPost]
+        [Route("GetAppObject")]
+        public string GetAppObject(string token)
+        {
+            token = TokenManager.readToken(HttpContext.Current.Request);
+
+            var strP = TokenManager.GetPrincipal(token);
+            if (strP != "0") //invalid authorization
+            {
+                return TokenManager.GenerateToken(strP);
+            }
+            else
+            {
+
+                using (ConsumerAssociationDBEntities entity = new ConsumerAssociationDBEntities())
+                {
+                    var appObjectList = entity.USR_APP_OBJECT.ToList();
+                    return TokenManager.GenerateToken(appObjectList);
+                }
+            }
+        }
+    }
+}
